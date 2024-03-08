b175aaebacf6259ff1938495530d57fcac1e63ac
isnendyankp <isnendyankp@gmail.com>
2024-03-09 06:23:55 +0700

    add base try except for product query

--- controllers/product.py
+++ controllers/product.py
@@ -5,7 +5,17 @@ product_routes = Blueprint('product_routes',__name__)
 
 @product_routes.route("/product", methods=['GET'])
 def product_home():
-    #
+
+    # Initialize Response Data
     response_data = dict()
+
+    # Fetch all Products
+    session = Session()
+
+    # try except
+
+    try:
+        product_query = select(Product)
+    except :
 
     return render_template("products/product_home.html")
