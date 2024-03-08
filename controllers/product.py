624c2950147f57e81d6f419c19618102f3394739
isnendyankp <isnendyankp@gmail.com>
2024-03-09 06:24:50 +0700

    Add Product model and import statements

--- controllers/product.py
+++ controllers/product.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template
 from connectors.mysql_connector import Session
 
+from models.product import Product
+from sqlalchemy import select
+
 product_routes = Blueprint('product_routes',__name__)
 
 @product_routes.route("/product", methods=['GET'])
